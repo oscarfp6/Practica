24b3fbf92887b458611f1d68dd20be6efea76bb7
oscarfp6 <oscarfuentespa6@gmail.com>
2025-10-08 18:25:07 +0200

    test creados

---
+++ MiLogicaTests/ModeloDatos/ActividadTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiLogica.ModeloDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiLogica.ModeloDatos.Tests
+{
+    [TestClass()]
+    public class ActividadTests
+    {
+        [TestMethod()]
+        public void ActividadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ActualizarMetricasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MiLogicaTests/ModeloDatos/UsuarioTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiLogica.ModeloDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiLogica.ModeloDatos.Tests
+{
+    [TestClass()]
+    public class UsuarioTests
+    {
+        [TestMethod()]
+        public void PermitirLoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UsuarioTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UsuarioTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PermitirLoginTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ComprobarPassWordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DesbloquearUsuarioTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VerificarInactividadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CambiarPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestablecerCuentaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ActualizarPerfilTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
